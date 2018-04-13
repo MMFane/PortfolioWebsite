ab331b1684050ff02de444a09efa105526200fe6
Sam Yeager <sam.williams@littlebytegames.com>
2018-04-13 11:55:47 -0700

    make initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var express         	= require("express"),
+    app             	= express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/wildtangent-casino", function(req, res) {
+	res.render("")
+});
+
+app.listen(3000, function() {
+    console.log("Portfolio: Server is go");
+});
