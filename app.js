2d2fd151160e6cb0a315fe7775f65b12985d9e94
Sam Yeager <samerinwilliams@yahoo.com>
2018-04-30 12:42:04 -0700

    Split site into multiple pages

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.get("/wildtangent-casino", function(req, res) {
-	res.render("")
+app.get("/resume", function(req, res) {
+	res.render("resume")
 });
+
+app.get("/about", function(req, res) {
+	res.render("about")
+});
+
+app.get("/contact", function(req, res) {
+	res.render("contact")
+});
+
+// app.get("/wildtangent-casino", function(req, res) {
+// 	res.render("")
+// });
 
 app.listen(port, function() {
     console.log("Portfolio: Server is go");
