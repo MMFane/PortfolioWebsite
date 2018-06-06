780d339abd9cf61fc3534840740055129595e15c
Sam <samerinwilliams@yahoo.com>
2018-06-05 21:29:10 -0700

    Add gallery lightbox, add vimeo iframe

--- public/scripts/masonry.js
+++ public/scripts/masonry.js
@@ -8,3 +8,15 @@ var $grid = $('.gallery-grid').imagesLoaded(function () {
         percentPosition: true
     });
 });
+
+var $images = $('.zoomable');
+$images.on("click", function () {
+    let newSrc = $(this).attr('src');
+    $('.zoom-image').css('background-image', `url(${newSrc}`);
+    $('.veil').css('display', 'block');
+    console.log(`${$('.veil').css('display')}`);
+});
+
+$('.veil').on("click", function () {
+    $(this).css('display', 'none');
+});
