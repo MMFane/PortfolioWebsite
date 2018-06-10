489d77471cc99ed7d62e2d52317c237b1683c6c9
Sam <samerinwilliams@yahoo.com>
2018-06-10 15:53:53 -0700

    Change gallery gutter to 6 columns, add scrolling through all images in lightbox functionality

--- public/scripts/masonry.js
+++ public/scripts/masonry.js
@@ -1,3 +1,4 @@
+// Masonry setup
 var $grid = $('.gallery-grid').imagesLoaded(function () {
     // init Masonry after all images have loaded
     $grid.masonry({
@@ -9,14 +10,62 @@ var $grid = $('.gallery-grid').imagesLoaded(function () {
     });
 });
 
-var $images = $('.zoomable');
+
+// lightbox functionality
+const $images = $('.zoomable');
+const btnLeft = $('#prev');
+const btnRight = $('#next');
+const veil = $('.veil');
+let imageArray = [];
+let ptr = 0;
+let currIdxText = $('#index');
+let totalNumImgs = $('#total');
+
+$images.each(function(index) {
+    imageArray.push( $(this).attr('src'));
+});
+const total = imageArray.length;
+totalNumImgs.text(`${total}`);
+
+// events
 $images.on("click", function () {
-    let newSrc = $(this).attr('src');
-    $('.zoom-image').css('background-image', `url(${newSrc}`);
-    $('.veil').css('display', 'block');
-    console.log(`${$('.veil').css('display')}`);
+    ptr = imageArray.indexOf($(this).attr('src'));
+    changeImage();
+    veil.css('display', 'block');
 });
 
-$('.veil').on("click", function () {
+btnLeft.on("click", function () {
+    event.stopPropagation();
+    if (ptr > 0) {
+        ptr--;
+    } else {
+        ptr = total - 1;
+    }
+    changeImage();
+});
+
+btnRight.on("click", function (event) {
+    event.stopPropagation();
+    if (ptr < total - 1) {
+        ptr++;    
+    } else {
+        ptr = 0;
+    }
+    changeImage();
+});
+
+veil.on("click", function () {
     $(this).css('display', 'none');
 });
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode == 27 && veil.css('display', 'block')) {
+        veil.css('display', 'none'); 
+    }
+});
+
+// helper functions
+function changeImage() {
+    currIdxText.text(`${ptr + 1}`);
+    $('.zoom-image').css('background-image', `url(${imageArray[ptr]}`);
+}
